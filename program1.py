cb5939e7e1857c3b1999c61e985ae9ec1e3d4292
HarshKesh <117992936+HarshKesh@users.noreply.github.com>
2024-10-07 14:16:52 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,20 +1,14 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
 
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {")":"(", "}":"{", "]":"["}
 
-
-
-
-
-
-    
-
-
-
-  
-
+        for char in s:
+            if char in mapping.values():
+                stack.append(char)
+            elif char in mapping.keys():
+                if not stack or mapping[char] != stack.pop():
+                    return False
+        
+        return not stack
