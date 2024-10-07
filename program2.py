f5368e243e78cbfe2c533fb727132d5c076af45c
HarshKesh <117992936+HarshKesh@users.noreply.github.com>
2024-10-07 14:20:46 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,10 +1,20 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        res = 0
+        roman = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
 
+        for a, b in zip(s, s[1:]):
+            if roman[a] < roman[b]:
+                res -= roman[a]
+            else:
+                res += roman[a]
 
-
+        return res + roman[s[-1]] 
